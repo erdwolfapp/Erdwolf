c0a1fa1d629261b2901113df232c425a85835577
alex4401 <rylatgl@gmail.com>
2019-11-19 16:25:17 +0100

    Add database.toml struct and stub for MigrateDatabase

--- app/database.go
+++ app/database.go
@@ -1,9 +1,18 @@
 package app
 
+import (
+	"errors"
+	"fmt"
+)
+
 type DatabaseConfig struct {
-	EnableAutoMigrations	bool	`toml:"database.enable-auto-migratons"`
+	EnableAutoMigrations	bool	`toml:"database.enable-auto-migrations"`
 	Driver		 			string 	`toml:"database.driver"`
 	// SQLite
 	Path					string	`toml:"database.sqlite.path"`
 }
 
+func (a *Application) MigrateDatabase() error {
+	fmt.Println("==> Running database migration...")
+	return errors.New("fixme: TODO: Implement MigrateDatabase()")
+}
